2deb1cbf938af1b21c9067a88899605189bc8534
Mauro Tomasoni <mauro.tom.vn@gmail.com>
2021-08-31 16:54:28 +0200

    add input from html form

--- pari_e_dispari/script.js
+++ pari_e_dispari/script.js
@@ -11,22 +11,34 @@ function isEven(num) {
   return false
 }
 
-// L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.;
-var userChoise = prompt('pari o dispari?');
-var userNumber = parseInt(prompt('scegli un numero da 1 a 5'));
+//al click di gioca
+var btnGioca = document.getElementById('gioca')
+btnGioca.addEventListener('click',
+function () {
+  // L’utente sceglie pari o dispari e ciclo il radio input per ricavare il value;
+  var userChoiseNode = document.getElementsByName('user-choise');
+  for (var i = 0; i < userChoiseNode.length; i++) {
+    if (userChoiseNode[i].checked) {
+      var userChoise = userChoiseNode[i].value
+    }
+  }
+  // L'utente inserisce un numero da 1 a 5
+  var userNumber = parseInt(document.getElementById('user-number').value);
+  // Generiamo un numero random(sempre da 1 a 5) per il computer(usando una funzione).;
+  var cpuNumber = randomNumber(1, 5);
 
-// Generiamo un numero random(sempre da 1 a 5) per il computer(usando una funzione).;
-var cpuNumber = randomNumber(1, 5);
+  // Sommiamo i due numeri;
+  var somma = userNumber + cpuNumber
 
-// Sommiamo i due numeri;
-var somma = userNumber + cpuNumber
+  // Stabiliamo se la somma dei due numeri è pari o dispari(usando una funzione);
+  // Dichiariamo chi ha vinto.;
+  if ((userChoise == 'pari' && isEven(somma)) || (userChoise == 'dispari' && !isEven(somma))) {
+    alert('hai vinto')
+  } else {
+    alert('hai perso')
+  }
 
-// Stabiliamo se la somma dei due numeri è pari o dispari(usando una funzione);
-// Dichiariamo chi ha vinto.;
-if ((userChoise == 'pari' && isEven(somma)) || (userChoise == 'dispari' && !isEven(somma))) {
-  alert('hai vinto')
-} else {
-  alert('hai perso')
-}
+  console.log(`user: ${userChoise} ${userNumber}, cpu: ${cpuNumber}, somma: ${somma}`);
+  }
+)
 
-console.log(`user: ${userChoise} ${userNumber}, cpu: ${cpuNumber}, somma: ${somma}`);

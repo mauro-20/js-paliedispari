aad1be8e89dc210789a3aafec0f3e34796d611a5
Mauro Tomasoni <mauro.tom.vn@gmail.com>
2021-09-01 17:09:12 +0200

    add inputs control

--- pari_e_dispari/script.js
+++ pari_e_dispari/script.js
@@ -14,45 +14,59 @@ function isEven(num) {
 //al click di gioca
 var btnGioca = document.getElementById('gioca')
 btnGioca.addEventListener('click',
-function () {
-  // L’utente sceglie pari o dispari e ciclo il radio input per ricavare il value;
-  var userChoiseNode = document.getElementsByName('user-choise');
-  for (var i = 0; i < userChoiseNode.length; i++) {
-    if (userChoiseNode[i].checked) {
-      var userChoise = userChoiseNode[i].value
+  function () {
+    // L’utente sceglie pari o dispari e ciclo il radio input per ricavare il value;
+    var userChoiseNode = document.getElementsByName('user-choise');
+    for (var i = 0; i < userChoiseNode.length; i++) {
+      if (userChoiseNode[i].checked) {
+        var userChoise = userChoiseNode[i].value
+      }
     }
-  }
-  // L'utente inserisce un numero da 1 a 5
-  var userNumber = parseInt(document.getElementById('user-number').value);
-  // Generiamo un numero random(sempre da 1 a 5) per il computer(usando una funzione).;
-  var cpuNumber = randomNumber(1, 5);
-
-  // Sommiamo i due numeri;
-  var somma = userNumber + cpuNumber
-  var sommaEvenOdd = ''
-  // Stabiliamo se la somma dei due numeri è pari o dispari(usando una funzione);
-  if (isEven(somma)) {
-    sommaEvenOdd = 'pari'
-  } else {
-    sommaEvenOdd = 'dispari'
-  }
+    // controllo selezione pari/dispari
+    if (userChoise != 'pari' && userChoise != 'dispari') {
+      alert('Seleziona pari/dispari e prova di nuovo')
+    } else {
     
-  // Dichiariamo chi ha vinto e stampo i risultati;
-  var winner = document.getElementById('winner')
-  
-  if ((userChoise == 'pari' && isEven(somma)) || (userChoise == 'dispari' && !isEven(somma))) {
-    winner.innerHTML = 'HAI VINTO'
-  } else {
-    winner.innerHTML = 'HAI PERSO'
-  }
-  
-  document.getElementById('user-choise-out').innerHTML = userChoise
-  document.getElementById('user-number-out').innerHTML = userNumber
-  document.getElementById('cpu-number-out').innerHTML = cpuNumber
-  document.getElementById('somma').innerHTML = somma;
-  document.getElementById('somma-even-odd').innerHTML = sommaEvenOdd
+      console.log(userChoise);
+      // L'utente inserisce un numero da 1 a 5
+      var userNumber = parseInt(document.getElementById('user-number').value);
+      // controllo selezione numero
+      if (isNaN(userNumber)) {
+        alert('Scegli un numero!')
+      } else {
 
-  document.getElementById('result').classList.add('visible')
+        
+        // Generiamo un numero random(sempre da 1 a 5) per il computer(usando una funzione).;
+        var cpuNumber = randomNumber(1, 5);
+        
+        // Sommiamo i due numeri;
+        var somma = userNumber + cpuNumber
+        var sommaEvenOdd = ''
+        // Stabiliamo se la somma dei due numeri è pari o dispari(usando una funzione);
+        if (isEven(somma)) {
+          sommaEvenOdd = 'pari'
+        } else {
+          sommaEvenOdd = 'dispari'
+        }
+        
+        // Dichiariamo chi ha vinto e stampo i risultati;
+        var winner = document.getElementById('winner')
+        
+        if ((userChoise == 'pari' && isEven(somma)) || (userChoise == 'dispari' && !isEven(somma))) {
+          winner.innerHTML = 'HAI VINTO'
+        } else {
+          winner.innerHTML = 'HAI PERSO'
+        }
+        
+        document.getElementById('user-choise-out').innerHTML = userChoise
+        document.getElementById('user-number-out').innerHTML = userNumber
+        document.getElementById('cpu-number-out').innerHTML = cpuNumber
+        document.getElementById('somma').innerHTML = somma;
+        document.getElementById('somma-even-odd').innerHTML = sommaEvenOdd
+        
+        document.getElementById('result').classList.add('visible')
+      }
+    }
   }
 )
 
